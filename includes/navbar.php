baa6df57e425672ffe39234f6eba6f36c75cbfb3
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-06 10:11:19 -0600

    Cracion del Navegador

---
+++ includes/navbar.php
@@ -0,0 +1,78 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">REGISTRO DE APLICACIONES</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Aplicaciones
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_marin/vistas/aplicaciones/index.php">Ingresar aplicacion</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_marin/vistas/aplicaciones/buscar.php">Buscar aplicacion</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Oficiales
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/programadores/index.php">Ingresar Oficiales</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/programadores/buscar.php">Buscar Oficiales</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Asignacion de Oficiales a APPS
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/asigna_programadores/index.php">Asignar Oficiales a las APPS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/asigna_programadores/buscar.php">Buscar Oficiales asignados a las APPS</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Problemas a realizar en las aplicaciones
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/tareas/index.php">Ingresar Problemas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/tareas/buscar.php">Buscar Problemas</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Problemas Reportados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                           
+                            <a class="nav-link" href="/alvarado_recuperacion/vistas/progreso_aplicacion/progreso_aplicacion.php">Buscar Problemas Reportados</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
