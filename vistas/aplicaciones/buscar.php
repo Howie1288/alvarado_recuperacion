eb3c031efa824ee2b43275224968e3e8fb4cdfb6
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-06 13:44:22 -0600

    Actualizacion

--- vistas/aplicaciones/buscar.php
+++ vistas/aplicaciones/buscar.php
@@ -24,14 +24,14 @@ require_once '../../modelos/Aplicacion.php';
         <div class="row justify-content-center">
             <form action="/alvarado_recuperacion/controladores/aplicaciones/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
             <div class="mb-3">
-                    <label for="aplicacion_nombre" class="form-label">Ingrese el nombre de la Aplicacion</label>
-                    <input type="text" name="aplicacion_nombre" id="aplicacion_nombre" class="form-control">
+                    <label for="apli_nombre" class="form-label">Ingrese el nombre de la Aplicacion</label>
+                    <input type="text" name="apli_nombre" id="apli_nombre" class="form-control">
                 </div>
 
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="aplicacion_fecha">Fecha de la aplicacion</label>
-                        <input type="date"  name="aplicacion_fecha" id="aplicacion_fecha" class="form-control">
+                        <label for="apli_fecha">Fecha de la aplicacion</label>
+                        <input type="date"  name="apli_fecha" id="apli_fecha" class="form-control">
                     </div>
                 </div>
                
--- vistas/oficiales/index.php
+++ vistas/oficiales/index.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL);
 <div class="container">
     <h1 class="text-center">Formulario de ingreso de Oficiales</h1>
     <div class="row justify-content-center">
-        <form action="/alvarado_recuperacion/controladores/programadores/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+        <form action="/alvarado_recuperacion/controladores/oficiales/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
             <div class="row mb-3">
                 <div class="col">
                     <label for="offi_grado">Grado del Oficial</label>
