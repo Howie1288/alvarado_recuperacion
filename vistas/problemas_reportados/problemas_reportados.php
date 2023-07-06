0c8777ca03f96bed77e1134a3b1126a5eceb812c
¨Howard <programadorhaaj.2023@gmail.com>
2023-07-06 11:38:32 -0600

    Creacion del archivo para reportar los nombres

---
+++ vistas/problemas_reportados/problemas_reportados.php
@@ -0,0 +1,39 @@
+<?php
+require_once '../../modelos/Aplicacion.php';
+
+try {
+    $aplicacion = new Aplicacion();
+    $aplicaciones = $aplicacion->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
+?>
+
+<?php include_once '../../includes/header.php' ?>
+<?php include_once '../../includes/navbar.php' ?>
+<div class="container">
+    <h1 class="text-center">Formulario de búsqueda deProblemas de Aplicaciones</h1>
+    <div class="row justify-content-center">
+        <form action="/alvarado_recuperacion/controladores/progreso_aplicacion/progreso_aplicacion.php" class="col-lg-8 border bg-light p-3">
+            <div class="row mb-3">
+                <div class="col">
+                    <label for="apli_id">Aplicación</label>
+                    <select name="apli_id" id="apli_id" class="form-control">
+                        <option value="">SELECCIONE...</option>
+                        <?php foreach ($aplicaciones as $key => $aplicacion) : ?>
+                            <option value="<?= $aplicacion['APLI_ID'] ?>"><?= $aplicacion['APLI_NOMBRE'] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col">
+                    <button type="submit" class="btn btn-info w-100">Buscar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<?php include_once '../../includes/footer.php' ?>
